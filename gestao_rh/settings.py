db6331daf25d85aebb6e645efba55a51eff59622
Levi Cruz <levicruz49@gmail.com>
2018-12-06 16:42:57 -0200

    criando api's

--- gestao_rh/settings.py
+++ gestao_rh/settings.py
@@ -22,6 +22,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'bootstrapform',
+    'rest_framework',
     'apps.core',
     'apps.empresas',
     'apps.funcionarios',
